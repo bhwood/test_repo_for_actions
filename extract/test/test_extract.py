2b1df31d622d66caede1e65c457355ad677dcb98
Billy Hopkinson-Wood <billy@bhwood.dev>
2024-05-15 16:14:42 +0100

    extract

--- extract/test/test_extract.py
+++ extract/test/test_extract.py
@@ -5,6 +5,7 @@ import pytest
 from moto import mock_aws
 from extract.src.extract import retrieve_timestamps
 
+
 @pytest.fixture(scope="class")
 def aws_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = "test"
@@ -21,30 +22,26 @@ def ssm_client(aws_credentials):
 
 
 def test_table_name_does_not_exist(ssm_client):
-    
+
     with pytest.raises(KeyError):
-        retrieve_timestamps('non_existent_table')
+        retrieve_timestamps("non_existent_table")
+
 
 def test_successful_retrieval(ssm_client):
 
     ssm_client.put_parameter(
-        Name='example_table',
-        Value='2024-05-14T12:00:00',
-        Type='String'
+        Name="example_table", Value="2024-05-14T12:00:00", Type="String"
     )
-    
+
     ssm_client.put_parameter(
-        Name='example_table',
-        Value='2024-05-15T13:00:00',
-        Type='String',
-        Overwrite=True
-    )    
+        Name="example_table", Value="2024-05-15T13:00:00", Type="String", Overwrite=True
+    )
 
-    timestamp = retrieve_timestamps('example_table')
+    timestamp = retrieve_timestamps("example_table")
     assert timestamp == "2024-05-15T13:00:00"
-    
 
-#TODO connection errors should be checked when connecting with credentials 
+
+# TODO connection errors should be checked when connecting with credentials
 # def test_connection_issue(ssm_client):
 #     with pytest.raises(ConnectionError):
 #         ssm_client.get_parameter(Name='example_table')
